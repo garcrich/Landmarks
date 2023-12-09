6752d4c86b74221ee180181c61cc21244abbdbca
Ricky <rg3646@outlook.com>
2023-12-09 12:51:29 -0700

    Adds detail page, mock data, and schema

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y:-130)
+                .padding(.bottom, -130)
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
@@ -21,7 +26,17 @@ struct ContentView: View {
                     Text("California")
                         .font(.subheadline)
                 }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }.padding()
+            
+            Spacer()
+            
         }
     }
 }
